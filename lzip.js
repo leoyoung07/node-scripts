56b40c2f099ab3ed610154b8ba2a5fcabdeee586
Leo Young <420760135@qq.com>
2018-04-01 22:50:07 +0800

    feat: lzip handle file and directory

--- lzip.js
+++ lzip.js
@@ -1,26 +1,44 @@
 #! /usr/bin/env node
 
-const program = require('commander');
-const inquirer = require('inquirer');
-const chalk = require('chalk');
-var fs = require('fs');
-var archiver = require('archiver');
-
+const program = require("commander");
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const fs = require("fs");
+const archiver = require("archiver");
+const path = require("path");
 
 program
-  .arguments('<srcPath>')
-  .arguments('<destFile>')
+  .arguments("<srcPath>")
+  .arguments("<destFile>")
   .action((srcPath, destFile) => {
-    console.log(srcPath, destFile);
+    if (srcPath.startsWith("/")) {
+      srcPath = path.resolve(srcPath);
+    } else {
+      srcPath = path.resolve(__dirname, srcPath);
+    }
+    if (destFile.startsWith("/")) {
+      destFile = path.resolve(destFile);
+    } else {
+      destFile = path.resolve(__dirname, destFile);
+    }
+    console.log('zipping files...');
+    console.log('srcPath: ' + srcPath);
+    console.log('destFile: ' + destFile);
     // create a file to stream archive data to.
-    var output = fs.createWriteStream(__dirname + '/' + destFile);
-    var archive = archiver('zip', {
+    var output = fs.createWriteStream(destFile);
+    var archive = archiver("zip", {
       zlib: { level: 9 } // Sets the compression level.
     });
     // pipe archive data to the file
     archive.pipe(output);
-    // append files from a sub-directory and naming it `new-subdir` within the archive
-    archive.directory(srcPath, srcPath);
+    if (fs.statSync(srcPath).isDirectory()) {
+      var dirname = srcPath.replace(path.dirname(srcPath), '');
+      // append files from a sub-directory and naming it `new-subdir` within the archive
+      archive.directory(srcPath, dirname);
+    } else {
+      var filename = path.basename(srcPath);
+      archive.file(srcPath, {name: filename});
+    }
     archive.finalize();
   });
 
